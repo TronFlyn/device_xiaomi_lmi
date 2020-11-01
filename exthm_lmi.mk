4d62a1e7ed4291704c1bb273fce2de9b428a9f6e
TronFlyn <ai974088741@outlook.com>
2020-11-01 11:05:45 +0800

    fix vendor/exthm path

--- exthm_lmi.mk
+++ exthm_lmi.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common exthm stuff.
-$(call inherit-product, vendor/config/common_full_phone.mk)
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
 PRODUCT_NAME := exthm_lmi
 PRODUCT_DEVICE := lmi
