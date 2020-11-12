358e2adccd938d78005d3f08c8cd12bef0df6070
TronFlyn <ai974088741@outlook.com>
2020-11-12 19:24:48 +0800

    pbrp

--- omni_lmi.mk
+++ omni_lmi.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/xiaomi/lmi
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 #PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
